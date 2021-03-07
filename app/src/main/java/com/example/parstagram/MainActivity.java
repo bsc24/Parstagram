540a2cde6e40d5a141711d525e7538ef6fb82976
bsc24 <bchin08@gmail.com>
2021-03-07 03:39:53 -0500

    Added bottom navigation bar; functionality will be implemented in the near future.

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.parstagram;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +15,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.parstagram.models.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView ivPostImage;
     private Button btnSubmit;
     private Button btnLogout;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
         etDescription = findViewById(R.id.etDescription);
         btnCapturePicture = findViewById(R.id.btnCapture);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnLogout = findViewById(R.id.btnLogout);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         btnCapturePicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,8 +68,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ivPostImage = findViewById(R.id.ivPostImage);
-        btnSubmit = findViewById(R.id.btnSubmit);
 
         queryPosts();
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -83,13 +90,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnLogout = findViewById(R.id.btnLogout);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "btnLogout onClick");
                 logout();
+            }
+        });
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "action_home", Toast.LENGTH_SHORT).show();
+                        //fragment = fragment1;
+                        break;
+                    case R.id.action_compose:
+                        Toast.makeText(MainActivity.this, "action_compose", Toast.LENGTH_SHORT).show();
+                        //fragment = fragment2;
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(MainActivity.this, "action_profile", Toast.LENGTH_SHORT).show();
+                    default:
+                        //fragment = fragment3;
+                        break;
+                }
+                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
     }
